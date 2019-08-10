24f8997cd98c46d79d62897157951fc5abc61c60
zealshen <zealshen@outlook.com>
2019-08-10 21:40:23 +0100

    Add Gini Index Feature Selection inside.

---
+++ Statistical_Based/Gini_Index/TestGiniIndex.py
@@ -0,0 +1,21 @@
+import scipy.io
+from sklearn import svm
+
+mat = scipy.io.loadmat('/home/zealshen/DATA/DATAfromASU/BiologicalData/colon.mat')
+X = mat['X']
+X = X.astype(float)
+y = mat['Y']
+y = y[:, 0]
+n_samples, n_features = X.shape
+
+# from skfeature.function.statistical_based import gini_index
+# score = gini_index.gini_index(X, y)
+# idx = gini_index.feature_ranking(score)
+# print(score)
+# print(idx)
+
+from Statistical_Based.Gini_Index import GiniIndexZeal
+score = GiniIndexZeal.gini_index(X, y)
+idx = GiniIndexZeal.feature_ranking(score)
+print(score)
+print(idx)
