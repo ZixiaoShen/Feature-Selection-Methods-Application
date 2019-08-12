91cc7c603ed60854884dfa0b2f68b7759434b2bb
zealshen <zealshen@outlook.com>
2019-08-12 21:52:16 +0100

    Add ReliefF feature selection method.

--- Similarity_Based/ReliefF/ReliefF.py
+++ Similarity_Based/ReliefF/ReliefF.py
@@ -28,8 +28,66 @@ def reliefF(X, y, **kwargs):
         near_hit = []
         near_miss = dict()
 
+        self_fea = X[idx, :]
         c = np.unique(y).tolist()
 
+        stop_dict = dict()
+        for label in c:
+            stop_dict[label] = 0
+        del c[c.index(y[idx])]
+
+        p_dict = dict()
+        p_label_idx = float(len(y[y == y[idx]]))/float(n_samples)  # the ratio of the same class
+
+        for label in c:
+            p_label_c = float(len(y[y == label]))/float(n_samples)
+            p_dict[label] = p_label_c/(1 - p_label_idx)
+            near_miss[label] = []
+
+        distance_sort = []
+        distance[idx, idx] = np.max(distance[idx, :])
+
+        for i in range(n_samples):
+            distance_sort.append([distance[idx, i], int(i), y[i]])
+        distance_sort.sort(key=lambda x: x[0])
+
+        for i in range(n_samples):
+            # find k nearest hit points
+            if distance_sort[i][2] == y[idx]:
+                if len(near_hit) < k:
+                    near_hit.append(distance_sort[i][1])
+                elif len(near_hit) == k:
+                    stop_dict[y[idx]] = 1
+            else:
+                # find k nearest miss points for each label
+                if len(near_miss[distance_sort[i][2]]) < k:
+                    near_miss[distance_sort[i][2]].append(distance_sort[i][1])
+                else:
+                    if len(near_miss[distance_sort[i][2]]) == k:
+                        stop_dict[distance_sort[i][2]] = 1
+            stop = True
+            for (key, value) in stop_dict.items():
+                    if value != 1:
+                        stop = False
+            if stop:
+                break
+
+        # update reliefF score
+        near_hit_term = np.zeros(n_features)
+        for ele in near_hit:
+            near_hit_term = np.array(near_hit_term) + np.array(abs(self_fea - X[ele, :]))
+
+        near_miss_term = dict()
+        for (label, miss_list) in near_miss.items():
+            near_miss_term[label] = np.zeros(n_features)
+            for ele in miss_list:
+                near_miss_term[label] = np.array(abs(self_fea - X[ele, :])) + np.array(near_miss_term[label])
+            score += near_miss_term[label]/(k*p_dict[label])
+        score -= near_hit_term / k
+    return score
 
 
+def feature_ranking(score):
+    idx = np.argsort(score, 0)
+    return idx[::-1]
 
